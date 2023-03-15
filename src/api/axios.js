b2d29517f62315309a2842e553c1f0a9fcc39c4e
Hwa-J <hj1103.code@gmail.com>
2023-03-15 23:35:44 +0900

    fix: baseURL 변경

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-	baseURL: 'https://youtube.googleapis.com/youtube/v3',
+	baseURL: 'https://www.googleapis.com/youtube/v3',
 	params: {
 		key: import.meta.env.VITE_YOUTUBE_API_KEY,
 	},
