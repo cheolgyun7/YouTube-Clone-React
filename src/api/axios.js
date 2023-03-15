011f0831b311c8d401ebd270e3c0c7eda3170dcc
cheolgyun7 <https://github.com/cheolgyun7/React_Studying.git>
2023-03-15 23:27:12 +0900

    feat:videocard

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,20 +1,10 @@
 import axios from 'axios'
 
-export const api = axios.create({
-	baseURL: 'https://www.googleapis.com/youtube/v3',
-
+const api = axios.create({
+	baseURL: 'https://youtube.googleapis.com/youtube/v3',
 	params: {
 		key: import.meta.env.VITE_YOUTUBE_API_KEY,
 	},
 })
 
-// export const getChannelImg = async (channelId) => {
-// 	const response = await instance.get('/channels', {
-// 		params: {
-// 			part: 'snippet',
-// 			id: channelId,
-// 		},
-// 	});
-
-// 	return response.data.items[0].snippet.thumbnails.default.url;
-// };
+export default api
